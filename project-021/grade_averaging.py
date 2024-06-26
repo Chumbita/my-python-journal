54e3b80dee2df7005ef1b1e4cf3f2ca2223b83e2
Chumbita <chumbitalexis@gmail.com>
2024-06-26 09:51:47 -0300

    Ejercicio programación funcional

--- project-021/grade_averaging.py
+++ project-021/grade_averaging.py
@@ -1,19 +1,19 @@
 import os
-notas = {"Mario Carriz": [1,2,3],"Chumbita Luciano": [4,5,6]}
+students_scores = {}
 
 def upload_notes():
   flag = True
-  scores = []
   print("-----|UPLOAD NOTES|-----\n")
-  a = int(input("Enter the amounts of evaluative grades your course has: "))
+  amount_notes = int(input("Enter the amounts of evaluative grades your course has: "))
   while flag:
+    scores = []
     os.system('cls')
     print("-----|UPLOAD NOTES|-----\n")
     student_name = input("Studen Name: ")
-    for i in range(a):
+    for i in range(amount_notes):
       x = float(input(f"Score {i+1}: "))
       scores.append(x)
-    notas[student_name] = scores
+    students_scores[student_name] = scores
 
     print("\nStudeny and scores uploaded successfully.")
     opc = input("\nDo you want to add another student? Y/N format: ")
@@ -23,33 +23,58 @@ def upload_notes():
       print("ERROR. Invalid Option.")
 
 def show_notes():
-  if notas: 
-    f_s = notas.keys()
-    notes_amount = len(notas[f_s[0]])
-    text = f"{'STUDENT':<20}"
-    for i in range(notes_amount - 1):
-      text += f'NOTA {i} '
-    print(text)
+  if students_scores: 
+    mykeys = list(students_scores.keys())
+    first_student = mykeys[0]
+    notes_amount = len(students_scores[first_student])
+    txt = f"{'STUDENT':<25}"
+    for i in range(notes_amount):
+      txt += f'NOTA {i+1} '
+    print(txt)
 
-    for s in notas.keys():
-      txt_input = f'{s:<21}'
-      for a in notas[s]:
-        txt_input += f'{a:<6}'
+    for student in students_scores.keys():
+      txt_input = f'{student:<26}'
+      for score in students_scores[student]:
+        txt_input += f'{score:<7}'
       print(txt_input)
+  else: 
+    print("There are nothing to show...")
+
+def average_notes():
+  if students_scores: 
+    mykeys = list(students_scores.keys())
+    first_student = mykeys[0]
+    notes_amount = len(students_scores[first_student])
+    print(f"{'STUDENT':<25}{'NOTA FINAL'}")
+
+    for student in students_scores.keys():
+      txt_input = f'{student:<28}'
+      average = 0
+      for score in students_scores[student]:
+        average += score
+      average /= notes_amount
+      print(f"{txt_input}{average:.2f}")
+  else: 
+    print("There are nothing to show...")
   
-def main():  
-  print("-----|UNIVERSITY SYSTEM|-----\n-1) UPLOAD COURSE NOTES.\n-2) SHOW COURSE NOTES.\n.3) CALCULATE AVERAGE.\n-4) EXIT.")
-  operation = int(input(f"Choose an operation: "))
-  os.system('cls')
-  if operation == 1:
-    upload_notes()
-  elif operation == 2:
-    show_notes()
-  # elif operation == 3:
-  #   average_notes()
-  else:
-    print("ERROR. Invalid Operation.")
-  input("\nPRESS ENTER TO CONTINUE...")
+def main():
+  flag = True
+  while flag:  
+    print("-----|UNIVERSITY SYSTEM|-----\n1) UPLOAD COURSE NOTES.\n2) SHOW COURSE NOTES.\n3) CALCULATE AVERAGE.\n4) EXIT.")
+    operation = int(input(f"Choose an operation: "))
+    os.system('cls')
+    if operation == 1:
+      upload_notes()
+    elif operation == 2:
+      show_notes()
+    elif operation == 3:
+      average_notes()
+    elif operation == 4: 
+      flag = False
+    else:
+      print("ERROR. Invalid Operation.")
+    input("\nPRESS ENTER TO CONTINUE...")
+    os.system('cls')
 
 if __name__ == '__main__':
   main()  
---
+++ project-021/read_me.txt
@@ -0,0 +1,13 @@
+Proyecto 21:
+Este programa es un promediador de notas de un curso académico. Ese programa cuenta con tres opciones que el usuario podrá realizar:
+    1. Cargar nombre del estudiante y sus respectivas notas.
+    2. Mostrar listado de estudiantes con sus respectivas notas.
+    3. Mostrar listado notas finales en donde se muestra el estudiante y el promedio de sus notas. 
+En la opción (1) el usuario será capaz de ingresar la cantidad de notas evaluativas con las que cuenta su curso. La carga de estudiantes y sus notas se realizan uno por uno. Después de cada carga se le pregunta si desea ingresar otro estudiante.
+
+Proyecto 21:
+This program is a grade averaging tool for an academic course. It features three options that the user can perform:
+    1.Enter the student's name and their respective grades.
+    2.Display a list of students with their respective grades.
+    3.Display a final grades list showing each student and the average of their grades.
+In option (1), the user will be able to enter the number of evaluative grades in their course. The entry of students and their grades is done one by one. After each entry, the user is asked if they want to enter another student.
